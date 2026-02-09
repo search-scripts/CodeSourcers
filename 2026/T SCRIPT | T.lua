339e3cb4f7375ad3f402f5e2efb7552373c0491a
search-scripts <hidley.xg@gmail.com>
2026-02-08 22:46:54 -0300

    Add T SCRIPT | T.lua

--- 2026/T SCRIPT | T.lua
+++ 2026/T SCRIPT | T.lua
@@ -1,2 +1,2 @@
 -- Gostou do Script? Pesquise por mais Scripts no meu Site: https://ethus-scripts.vercel.app/
-loadstring(game:HttpGet("https://pastefy.app/hWDByPfk/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/BUOd3TiW/raw"))()
