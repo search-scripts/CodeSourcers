1410f0792ecb1a79014774e9ca2605984e19dcdc
search-scripts <hidley.xg@gmail.com>
2026-03-01 12:55:30 -0300

    Add Zinul SCRIPT | Tyb hub.lua

---
+++ 2026/Zinul SCRIPT | Tyb hub.lua
@@ -0,0 +1,2 @@
+-- Gostou do Script? Pesquise por mais Scripts no meu Site: https://ethus-scripts.vercel.app/
+loadstring(game:HttpGet("https://raw.githubusercontent.com/search-scripts/Globe/main/2026/Zinul SCRIPT | Tyb hub.lua"))()
