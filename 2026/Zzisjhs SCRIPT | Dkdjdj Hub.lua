0700c762a5a104de3cc5f4f21f79a556ab8ee51a
search-scripts <hidley.xg@gmail.com>
2026-03-01 12:57:43 -0300

    Add Zzisjhs SCRIPT | Dkdjdj Hub.lua

---
+++ 2026/Zzisjhs SCRIPT | Dkdjdj Hub.lua
@@ -0,0 +1,2 @@
+-- Gostou do Script? Pesquise por mais Scripts no meu Site: https://ethus-scripts.vercel.app/
+loadstring(game:HttpGet("https://raw.githubusercontent.com/search-scripts/Globe/main/2026/Zzisjhs%20SCRIPT%20%7C%20Dkdjdj%20Hub.lua"))()
