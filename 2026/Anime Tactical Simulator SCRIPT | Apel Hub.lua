55c8d53d3a73a28e162dff4900ded856d5010040
search-scripts <hidley.xg@gmail.com>
2026-03-02 13:51:55 -0300

    Add Anime Tactical Simulator SCRIPT | Apel Hub.lua

--- 2026/Anime Tactical Simulator SCRIPT | Apel Hub.lua
+++ 2026/Anime Tactical Simulator SCRIPT | Apel Hub.lua
@@ -1,2 +1,2 @@
 -- Gostou do Script? Pesquise por mais Scripts no meu Site: https://ethus-scripts.vercel.app/
-loadstring(game:HttpGet("https://pastefy.app/jb7yko18/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/Lm98DLn5/raw"))()
