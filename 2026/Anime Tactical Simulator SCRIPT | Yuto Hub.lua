61982d2b066a692f22194b2186fed260819ba459
search-scripts <hidley.xg@gmail.com>
2026-03-02 13:49:25 -0300

    Add Anime Tactical Simulator SCRIPT | Yuto Hub.lua

--- 2026/Anime Tactical Simulator SCRIPT | Yuto Hub.lua
+++ 2026/Anime Tactical Simulator SCRIPT | Yuto Hub.lua
@@ -1,2 +1,2 @@
 -- Gostou do Script? Pesquise por mais Scripts no meu Site: https://ethus-scripts.vercel.app/
-loadstring(game:HttpGet("https://pastefy.app/6PdOctP8/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/mBw4F8fU/raw"))()
