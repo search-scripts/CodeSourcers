230dec007caa62ce7e17f99a725963968a74a91e
search-scripts <hidley.xg@gmail.com>
2026-03-08 23:57:00 -0300

    Add Bizarre Lineage SCRIPT | Nameless Hub.lua

--- 2026/Bizarre Lineage SCRIPT | Nameless Hub.lua
+++ 2026/Bizarre Lineage SCRIPT | Nameless Hub.lua
@@ -1,2 +1,2 @@
 -- Gostou do Script? Pesquise por mais Scripts no meu Site: https://ethus-scripts.vercel.app/
-loadstring(game:HttpGet("https://pastefy.app/4gIoSyhW/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/qkYtWwQ0/raw"))()
