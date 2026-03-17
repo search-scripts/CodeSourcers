90db3e96f7d83a15d695a7ab5aaf7037e01e1bfd
search-scripts <hidley.xg@gmail.com>
2026-03-17 15:43:52 -0300

    Add King Legacy SCRIPT | Tsuo Hub.lua

--- 2026/King Legacy SCRIPT | Tsuo Hub.lua
+++ 2026/King Legacy SCRIPT | Tsuo Hub.lua
@@ -1,2 +1,2 @@
 -- Gostou do Script? Pesquise por mais Scripts no meu Site: https://ethus-scripts.vercel.app/
-loadstring(game:HttpGet("https://pastefy.app/eC7zdPbv/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/BWO0fIz7/raw"))()
