18f1a7667ca7b62ec9655467133b8249047b1627
search-scripts <hidley.xg@gmail.com>
2026-03-17 14:22:04 -0300

    Add King Legacy SCRIPT | Tsuo Hub.lua

--- 2026/King Legacy SCRIPT | Tsuo Hub.lua
+++ 2026/King Legacy SCRIPT | Tsuo Hub.lua
@@ -1,10 +1,2 @@
 -- Gostou do Script? Pesquise por mais Scripts no meu Site: https://ethus-scripts.vercel.app/
-getgenv().SCRIPT_KEY = "Script feito por Hidley William"
-
-loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/759168c594ac396233f61f2ddc3ed1b61bcea94d2e37c059e051013bd37f3b29/download"))()
-
--- King Legacy | Tsuo Hub
-loadstring(game:HttpGet("https://credential-request.vercel.app/Hub/Universal/System.lua"))()({
-    HubName = "Tsuo Hub",
-    Script = "https://pastefy.app/nfoK1Isp/raw"
-})
+loadstring(game:HttpGet("https://pastefy.app/eC7zdPbv/raw"))()
