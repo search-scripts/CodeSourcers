b3b6ae1406222816ca9b258ad5f6e7957726d1d5
search-scripts <hidley.xg@gmail.com>
2026-03-20 08:46:32 -0300

    Add Blox Fruits SCRIPT | Redz Hub.lua

--- 2026/Blox Fruits SCRIPT | Redz Hub.lua
+++ 2026/Blox Fruits SCRIPT | Redz Hub.lua
@@ -1,2 +1,2 @@
 -- Gostou do Script? Pesquise por mais Scripts no meu Site: https://ethus-scripts.vercel.app/
-loadstring(game:HttpGet("https://pastefy.app/Y1Cm0Dhl/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/2FXsq98V/raw"))()
