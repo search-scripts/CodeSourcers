6e24a4d0d655f391d1aaada0b5672d880ec0c684
search-scripts <hidley.xg@gmail.com>
2026-04-15 18:46:06 -0300

    Add Paradox SCRIPT | LH Hub.lua

--- 2026/Paradox SCRIPT | LH Hub.lua
+++ 2026/Paradox SCRIPT | LH Hub.lua
@@ -1,2 +1,2 @@
 -- Gostou do Script? Pesquise por mais Scripts no meu Site: https://ethus-scripts.vercel.app/
-loadstring(game:HttpGet("https://pastefy.app/mDZo24Jj/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/EE1kaFVe/raw"))()
