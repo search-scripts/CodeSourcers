cfa1fbda5908e1b1e777233718df48ae8d8dd630
search-scripts <hidley.xg@gmail.com>
2026-04-18 17:20:41 -0300

    Add Bizarre Lineage SCRIPT | Rail Hub.lua

--- 2026/Bizarre Lineage SCRIPT | Rail Hub.lua
+++ 2026/Bizarre Lineage SCRIPT | Rail Hub.lua
@@ -1,2 +1,2 @@
 -- Gostou do Script? Pesquise por mais Scripts no meu Site: https://ethus-scripts.vercel.app/
-loadstring(game:HttpGet("https://raw.githubusercontent.com/search-scripts/Globe/main/2026/Bizarre%20Lineage%20SCRIPT%20%7C%20Rail%20Hub.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Bliqe/Upload/refs/heads/main/Games/BL/Rail.lua"))()
