72ff060b7ebd08b39a45af691dfe8a8f0cf76e42
search-scripts <hidley.xg@gmail.com>
2026-04-19 10:48:00 -0300

    Add Bizarre Lineage SCRIPT | Aeonic Hub.lua

--- 2026/Bizarre Lineage SCRIPT | Aeonic Hub.lua
+++ 2026/Bizarre Lineage SCRIPT | Aeonic Hub.lua
@@ -1,2 +1,2 @@
 -- Gostou do Script? Pesquise por mais Scripts no meu Site: https://ethus-scripts.vercel.app/
-loadstring(game:HttpGet("https://pastefy.app/EGiSGWmm/raw"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Bliqe/Upload/refs/heads/main/Games/BL/Aeonic.lua"))()
