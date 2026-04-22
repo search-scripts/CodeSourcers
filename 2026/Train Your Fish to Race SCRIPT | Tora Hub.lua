3d29ee848e3d56f3bd31d0f60435342e945ea389
search-scripts <hidley.xg@gmail.com>
2026-04-22 18:49:22 -0300

    Add Train Your Fish to Race SCRIPT | Tora Hub.lua

--- 2026/Train Your Fish to Race SCRIPT | Tora Hub.lua
+++ 2026/Train Your Fish to Race SCRIPT | Tora Hub.lua
@@ -1,2 +1,2 @@
 -- Gostou do Script? Pesquise por mais Scripts no meu Site: https://ethus-scripts.vercel.app/
-loadstring(game:HttpGet("https://pastefy.app/sIkF1MVQ/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/9hJytZmt/raw"))()
