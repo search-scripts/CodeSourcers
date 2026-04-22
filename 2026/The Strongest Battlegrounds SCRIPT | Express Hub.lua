028ae3fbca937f90b461f93c1283061bf0aa920a
search-scripts <hidley.xg@gmail.com>
2026-04-22 18:41:14 -0300

    Add The Strongest Battlegrounds SCRIPT | Express Hub.lua

--- 2026/The Strongest Battlegrounds SCRIPT | Express Hub.lua
+++ 2026/The Strongest Battlegrounds SCRIPT | Express Hub.lua
@@ -1,2 +1,2 @@
 -- Gostou do Script? Pesquise por mais Scripts no meu Site: https://ethus-scripts.vercel.app/
-loadstring(game:HttpGet("https://pastefy.app/leJ5432J/raw"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Bliqe/Upload/refs/heads/main/Games/TSB/Express.lua"))()
