f5c22f635c9408e647ecc61e86e5f7988b08609c
search-scripts <hidley.xg@gmail.com>
2026-04-22 18:55:53 -0300

    Add Pull Lucky Blocks SCRIPT | Tora Hub.lua

--- 2026/Pull Lucky Blocks SCRIPT | Tora Hub.lua
+++ 2026/Pull Lucky Blocks SCRIPT | Tora Hub.lua
@@ -1,2 +1,2 @@
 -- Gostou do Script? Pesquise por mais Scripts no meu Site: https://ethus-scripts.vercel.app/
-loadstring(game:HttpGet("https://pastefy.app/BCjz9Whb/raw"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Bliqe/Upload/refs/heads/main/Games/PLB/Tora.lua"))()
