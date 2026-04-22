fec6de47abef2f55438c7cb185606dbddcdbf4f4
search-scripts <hidley.xg@gmail.com>
2026-04-22 18:31:26 -0300

    Add The Strongest Battlegrounds SCRIPT | Speed X Hub.lua

--- 2026/The Strongest Battlegrounds SCRIPT | Speed X Hub.lua
+++ 2026/The Strongest Battlegrounds SCRIPT | Speed X Hub.lua
@@ -1,2 +1,2 @@
 -- Gostou do Script? Pesquise por mais Scripts no meu Site: https://ethus-scripts.vercel.app/
-loadstring(game:HttpGet("https://pastefy.app/WoZChykL/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/9esZDZ8t/raw"))()
