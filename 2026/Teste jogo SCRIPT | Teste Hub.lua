e15bf558028a4934b33fd4bad8e19b566d8f08a4
search-scripts <hidley.xg@gmail.com>
2026-04-22 13:48:09 -0300

    Add Teste jogo SCRIPT | Teste Hub.lua

--- 2026/Teste jogo SCRIPT | Teste Hub.lua
+++ 2026/Teste jogo SCRIPT | Teste Hub.lua
@@ -1,2 +1,2 @@
 -- Gostou do Script? Pesquise por mais Scripts no meu Site: https://ethus-scripts.vercel.app/
-loadstring(game:HttpGet("https://pastefy.app/eRC7oAII/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/5E3lYwof/raw"))()
