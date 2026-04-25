b26185a3c1ef5d002ef0ccef6d98d03aea7b991f
search-scripts <hidley.xg@gmail.com>
2026-04-25 00:15:05 -0300

    Add Driving Empire SCRIPT | Aden Hub.lua

--- 2026/Driving Empire SCRIPT | Aden Hub.lua
+++ 2026/Driving Empire SCRIPT | Aden Hub.lua
@@ -1,2 +1,2 @@
 -- Gostou do Script? Pesquise por mais Scripts no meu Site: https://ethus-scripts.vercel.app/
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Bliqe/Upload/refs/heads/main/Games/DE/Aden.lua"))()
+loadstring(game:HttpGet("https://pastefy.app/bxPU8q1z/raw"))()
