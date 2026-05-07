779f3826d89674699e6453df1e1cf9ced26fa54a
search-scripts <hidley.xg@gmail.com>
2026-05-07 15:37:41 -0300

    Add Jujutsu Shenanigans SCRIPT | Flow Hub.lua

--- 2026/Jujutsu Shenanigans SCRIPT | Flow Hub.lua
+++ 2026/Jujutsu Shenanigans SCRIPT | Flow Hub.lua
@@ -1,2 +1,2 @@
 -- Gostou do Script? Pesquise por mais Scripts no meu Site: https://ethus-scripts.vercel.app/
-loadstring(game:HttpGet("https://pastefy.app/4SUQ8P85/raw"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Bliqe/Upload/refs/heads/main/Games/JS/Flow.lua"))()
