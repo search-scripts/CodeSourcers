b185dea4465026e0618348d1f6c88006b4e6f48b
search-scripts <hidley.xg@gmail.com>
2026-05-11 17:34:30 -0300

    Add [💥UPDATE 4] Attack on Titan Revolution 🇧🇷 SCRIPT | Airflow Hub.lua

--- 2026/[💥UPDATE 4] Attack on Titan Revolution 🇧🇷 SCRIPT | Airflow Hub.lua
+++ 2026/[💥UPDATE 4] Attack on Titan Revolution 🇧🇷 SCRIPT | Airflow Hub.lua
@@ -1,2 +1,2 @@
 -- Gostou do Script? Pesquise por mais Scripts no meu Site: https://ethus-scripts.vercel.app/
-loadstring(game:HttpGet("https://pastefy.app/LOytxEiM/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/0lR8HVLE/raw"))()
