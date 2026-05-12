016535603103cb263513c4b8ff62e269050be07c
search-scripts <hidley.xg@gmail.com>
2026-05-12 13:30:07 -0300

    Add [🔥Big Update⚔️] Sailor Piece SCRIPT | Teste Hub.lua

--- 2026/[🔥Big Update⚔️] Sailor Piece SCRIPT | Teste Hub.lua
+++ 2026/[🔥Big Update⚔️] Sailor Piece SCRIPT | Teste Hub.lua
@@ -1,2 +1,2 @@
 -- Gostou do Script? Pesquise por mais Scripts no meu Site: https://ethus-scripts.vercel.app/
-loadstring(game:HttpGet("https://pastefy.app/cZG8pKka/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/6OHc6qlC/raw"))()
