573034c83693868b48eb1cb097b251ca9307fa88
search-scripts <hidley.xg@gmail.com>
2026-05-11 22:43:36 -0300

    Add Kick a Lucky Block SCRIPT | Tora Hub.lua

--- 2026/Kick a Lucky Block SCRIPT | Tora Hub.lua
+++ 2026/Kick a Lucky Block SCRIPT | Tora Hub.lua
@@ -1,2 +1,2 @@
 -- Gostou do Script? Pesquise por mais Scripts no meu Site: https://ethus-scripts.vercel.app/
-loadstring(game:HttpGet("https://pastefy.app/oIwT3Pzp/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/bukE6i5M/raw"))()
