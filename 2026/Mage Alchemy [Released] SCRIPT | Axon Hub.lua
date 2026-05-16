107fce2036f326b9841a2b0527451291d1161274
search-scripts <hidley.xg@gmail.com>
2026-05-16 20:10:42 -0300

    Add Mage Alchemy [Released] SCRIPT | Axon Hub.lua

--- 2026/Mage Alchemy [Released] SCRIPT | Axon Hub.lua
+++ 2026/Mage Alchemy [Released] SCRIPT | Axon Hub.lua
@@ -1,2 +1,2 @@
 -- Gostou do Script? Pesquise por mais Scripts no meu Site: https://ethus-scripts.vercel.app/
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Bliqe/Upload/refs/heads/main/Games/MA[/Axon.lua"))()
+loadstring(game:HttpGet("https://pastefy.app/qRQnA2f7/raw"))()
