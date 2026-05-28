5bbf46226e3e003f60787370976d54060bc40190
search-scripts <hidley.xg@gmail.com>
2026-05-28 13:31:20 -0300

    Add [💥UPDATE 4] Attack on Titan Revolution 🇧🇷 SCRIPT | Titanic Hub.lua

--- 2026/[💥UPDATE 4] Attack on Titan Revolution 🇧🇷 SCRIPT | Titanic Hub.lua
+++ 2026/[💥UPDATE 4] Attack on Titan Revolution 🇧🇷 SCRIPT | Titanic Hub.lua
@@ -1,2 +1,2 @@
 -- Gostou do Script? Pesquise por mais Scripts no meu Site: https://ethus-scripts.vercel.app/
-loadstring(game:HttpGet("https://pastefy.app/LYKbrq3z/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/NAVYE12R/raw"))()
