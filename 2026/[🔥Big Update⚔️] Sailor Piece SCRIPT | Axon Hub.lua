2a814955f2fdc34feb858d4f77ef0343291d50f9
search-scripts <hidley.xg@gmail.com>
2026-06-05 16:59:56 -0300

    Add [🔥Big Update⚔️] Sailor Piece SCRIPT | Axon Hub.lua

--- 2026/[🔥Big Update⚔️] Sailor Piece SCRIPT | Axon Hub.lua
+++ 2026/[🔥Big Update⚔️] Sailor Piece SCRIPT | Axon Hub.lua
@@ -1,2 +1,2 @@
 -- Gostou do Script? Pesquise por mais Scripts no meu Site: https://ethus-scripts.vercel.app/
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Bliqe/Upload/refs/heads/main/Games/[USP/Axon.lua"))()
+loadstring(game:HttpGet("https://pastefy.app/yGtGITEA/raw"))()
