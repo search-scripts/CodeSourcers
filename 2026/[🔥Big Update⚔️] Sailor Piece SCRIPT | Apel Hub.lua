5d237a3e86daab925209eb620a376616d10f1f7e
search-scripts <hidley.xg@gmail.com>
2026-06-05 16:52:32 -0300

    Add [🔥Big Update⚔️] Sailor Piece SCRIPT | Apel Hub.lua

--- 2026/[🔥Big Update⚔️] Sailor Piece SCRIPT | Apel Hub.lua
+++ 2026/[🔥Big Update⚔️] Sailor Piece SCRIPT | Apel Hub.lua
@@ -1,2 +1,2 @@
 -- Gostou do Script? Pesquise por mais Scripts no meu Site: https://ethus-scripts.vercel.app/
-loadstring(game:HttpGet("https://pastefy.app/BVt5epYI/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/rHHzWwHv/raw"))()
