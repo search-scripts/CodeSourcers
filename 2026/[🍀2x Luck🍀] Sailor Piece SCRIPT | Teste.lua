fede5cf311fa906c6df719cecb1e5b92181b12a4
search-scripts <hidley.xg@gmail.com>
2026-06-05 17:49:10 -0300

    Add [🍀2x Luck🍀] Sailor Piece SCRIPT | Teste.lua

--- 2026/[🍀2x Luck🍀] Sailor Piece SCRIPT | Teste.lua
+++ 2026/[🍀2x Luck🍀] Sailor Piece SCRIPT | Teste.lua
@@ -1,2 +1,2 @@
 -- Gostou do Script? Pesquise por mais Scripts no meu Site: https://ethus-scripts.vercel.app/
-loadstring(game:HttpGet("https://pastefy.app/WDeA1Hnw/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/ySxKro0Z/raw"))()
