1e784261a021b5d053eeb038709aa8db4cf5f5f1
search-scripts <hidley.xg@gmail.com>
2026-06-05 23:09:17 -0300

    Add [🍀2x Luck🍀] Sailor Piece SCRIPT | Teste Hub.lua

--- 2026/[🍀2x Luck🍀] Sailor Piece SCRIPT | Teste Hub.lua
+++ 2026/[🍀2x Luck🍀] Sailor Piece SCRIPT | Teste Hub.lua
@@ -1,2 +1,2 @@
 -- Gostou do Script? Pesquise por mais Scripts no meu Site: https://ethus-scripts.vercel.app/
-loadstring(game:HttpGet("https://pastefy.app/Uh1Ix4eg/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/mZH0yYJd/raw"))()
