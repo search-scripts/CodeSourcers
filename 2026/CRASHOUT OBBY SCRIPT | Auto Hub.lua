3ecb458ba25f3e1def45fac477c5161f1c3061d7
search-scripts <hidley.xg@gmail.com>
2026-06-06 01:43:03 -0300

    Add CRASHOUT OBBY SCRIPT | Auto Hub.lua

--- 2026/CRASHOUT OBBY SCRIPT | Auto Hub.lua
+++ 2026/CRASHOUT OBBY SCRIPT | Auto Hub.lua
@@ -1,2 +1,2 @@
 -- Gostou do Script? Pesquise por mais Scripts no meu Site: https://ethus-scripts.vercel.app/
-loadstring(game:HttpGet("https://pastefy.app/LPK5SVbz/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/9tdvrLwS/raw"))()
