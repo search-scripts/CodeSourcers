d8d4138a6b547378a8246eb4fd9d7e06c067634d
search-scripts <hidley.xg@gmail.com>
2026-06-06 00:29:46 -0300

    Add [2x🍀] Broken Blade [Explosion] SCRIPT | Apel Hub.lua

--- 2026/[2x🍀] Broken Blade [Explosion] SCRIPT | Apel Hub.lua
+++ 2026/[2x🍀] Broken Blade [Explosion] SCRIPT | Apel Hub.lua
@@ -1,2 +1,2 @@
 -- Gostou do Script? Pesquise por mais Scripts no meu Site: https://ethus-scripts.vercel.app/
-loadstring(game:HttpGet("https://pastefy.app/GmFpsD75/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/WCmqszbD/raw"))()
