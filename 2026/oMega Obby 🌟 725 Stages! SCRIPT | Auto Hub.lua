04f87497278b97fa5b11d44d4850f6db8c842bf1
search-scripts <hidley.xg@gmail.com>
2026-06-06 01:44:11 -0300

    Add oMega Obby 🌟 725 Stages! SCRIPT | Auto Hub.lua

--- 2026/oMega Obby 🌟 725 Stages! SCRIPT | Auto Hub.lua
+++ 2026/oMega Obby 🌟 725 Stages! SCRIPT | Auto Hub.lua
@@ -1,2 +1,2 @@
 -- Gostou do Script? Pesquise por mais Scripts no meu Site: https://ethus-scripts.vercel.app/
-loadstring(game:HttpGet("https://pastefy.app/DBM4lZMa/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/1B9RtXxV/raw"))()
