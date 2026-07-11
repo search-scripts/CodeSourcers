8a95221fb9363464f7830204d57d3dcba1d92ad7
search-scripts <hidley.xg@gmail.com>
2026-07-11 01:03:44 -0300

    Add 97469657718.lua

---
+++ 2026/97469657718.lua
@@ -0,0 +1,2 @@
+-- Gostou do Script? Pesquise por mais Scripts no meu Site: https://ethus-scripts.vercel.app/
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Bliqe/Upload/refs/heads/main/Games/SODF/85499998210.lua"))()
