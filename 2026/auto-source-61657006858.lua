81508cedd4acbf815d6e8942a9394f712a74ecbf
search-scripts <hidley.xg@gmail.com>
2026-08-26 13:40:48 -0300

    Add auto source 61657006858.lua

---
+++ 2026/auto-source-61657006858.lua
@@ -0,0 +1,7 @@
+-- FIX SOON SLIENT AIM ESP FOV AIMBOT
+--- WARNING ITS BETA THERE WILL BE MORE OP FUTURE
+---- dont works on solara or delta
+
+loadstring(game:HttpGet("https://hackmanhub.pages.dev/loader.txt"))()
+
+---join our dc to get free keys and updates https://discord.gg/KmtXAzbDTy
