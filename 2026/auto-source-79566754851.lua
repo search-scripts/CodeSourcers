2f70c308362b072cba3b88807bbf377d254a8780
search-scripts <hidley.xg@gmail.com>
2026-08-26 20:52:42 -0300

    Add auto source 79566754851.lua

---
+++ 2026/auto-source-79566754851.lua
@@ -0,0 +1,7 @@
+-- Highest jump ever infinite money (fastest version)
+-- uh I made it faster than the other few scripts for the game 👍
+-- also it doesn't TP you anymore, you can just play the game normally
+
+-- (it might make your ping go up due to sending a TON of requests, just rejoin once you think you have enough money to fix the lag)
+
+loadstring("for i=1,25 do\n"..game:HttpGet("https://raw.githubusercontent.com/Ribbined/MyScripts/refs/heads/main/infmoney%20Highest%20Jump%20Ever.lua").."\nend")()
