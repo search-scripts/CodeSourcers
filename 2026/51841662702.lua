defc9d87ccc5e8f6502bd7655ff7ae26decf2e3c
search-scripts <hidley.xg@gmail.com>
2026-09-08 09:48:35 -0300

    Add 51841662702.lua

---
+++ 2026/51841662702.lua
@@ -0,0 +1,2 @@
+-- Gostou do Script? Pesquise por mais Scripts no meu Site: https://ethus-scripts.vercel.app/
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Bliqe/Upload/refs/heads/main/Games/AAA/16658269715.lua"))()
